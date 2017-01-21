e1913a988ba618c3eb4ad99830537ac73d155251
vector8 <vector8@localhost>
2017-01-21 15:35:53 -0500

    Finished work on the ripple mesh. Customized the standard 3rd person controller to work with it. Exported both as a package.

---
+++ MeshDeformerTest/Assets/Scripts/WaveGen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGen : MonoBehaviour
+{
+    public float scale = 0.1f;
+    public float speed = 1f;
+    public float noiseStrength = 1f;
+    public float noiseWalk = 1f;
+
+    private Vector3[] baseHeight;
+    private Mesh mesh;
+
+    // Use this for initialization
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        baseHeight = mesh.vertices;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] vertices = new Vector3[baseHeight.Length];
+
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertex = baseHeight[i];
+            vertex.y += Mathf.Sin(Time.time * speed + baseHeight[i].x + baseHeight[i].y + baseHeight[i].z) * scale;
+            vertex.y += Mathf.PerlinNoise(baseHeight[i].x + noiseWalk, baseHeight[i].y + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
+            vertices[i] = vertex;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
