2dd379d5448d61b21ac23a21b558ba789b31a64b
ishraf222 <ishraf222@localhost>
2017-01-22 15:14:57 -0500

    he farts when he jumps

--- Sumonami/Assets/AudioPlay.cs
+++ Sumonami/Assets/AudioPlay.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioPlay : MonoBehaviour {
-
+    AudioSource audio;
 	// Use this for initialization
 	void Start () {
-		
+        audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
