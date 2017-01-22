3ba6bcf2e8925a9f9651aefd7fff42e40deaf248
ishraf222 <ishraf222@localhost>
2017-01-22 12:07:38 -0500

    added the entire nature pack

---
+++ Sumonami/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+using UnityEngine.Events;
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent OnTriggerEnterEvent;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+        print("Working");
+
+        if (OnTriggerEnterEvent != null)
+        {
+            OnTriggerEnterEvent.Invoke();
+        }
+
+    }
+}
