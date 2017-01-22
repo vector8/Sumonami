baabbb217aa374771cbe36c7f1f71416153181e1
Joss <Joss@localhost>
2017-01-22 12:29:16 -0500

    Added script for dust trails when flying, particle prefabs for flying and sakura

---
+++ Sumonami/Assets/Particles/flyingParticleTrail.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyingParticleTrail : MonoBehaviour {
+
+    Rigidbody physicsBody;
+    ParticleSystem flyingParticleSystem;
+    public float triggerSpeed;
+    public float scaleByObjectSpeed;
+    public float maxScale;
+	// Use this for initialization
+	void Start ()
+    {
+        physicsBody = this.GetComponent<Rigidbody>();
+        flyingParticleSystem = this.GetComponentInChildren<ParticleSystem>();
+        flyingParticleSystem.Stop();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float speed = physicsBody.velocity.magnitude;
+        flyingParticleSystem.startSize = Mathf.Min(maxScale, scaleByObjectSpeed * speed);
+        if (flyingParticleSystem.isPaused && (speed >= triggerSpeed)) //does c# do lazy evaluation from left to right or right to left
+        {
+            flyingParticleSystem.Play();
+        } else flyingParticleSystem.Pause();
+
+    }
+}
